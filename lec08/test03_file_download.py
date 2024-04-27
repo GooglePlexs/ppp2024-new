7a95d9f277d3139d4c4b6a37f466f11d7c9535e7
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-04-27 04:32:47 +0000

    8주차 과제

--- lec08/test03_file_download.py
+++ lec08/test03_file_download.py
@@ -5,7 +5,7 @@ URL = "https://api.taegon.kr/stations/146/?sy=2022&ey=2022&format=csv"
 
 filename = "lec08/weather_146_2022.csv"
 
-if not os.path.exits(filename):  # 파일이 없으면 다운받는 코드(반복해서 긁어오는 것 방지 - 디도스 예방)
+if not os.path.exists(filename):  # 파일이 없으면 다운받는 코드(반복해서 긁어오는 것 방지 - 디도스 예방)
     with open(filename, "w") as f:
         res = requests.get(URL) 
         f.write(res.text)

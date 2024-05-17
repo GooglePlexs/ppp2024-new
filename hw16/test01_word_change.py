c8e87253c37b74c665c793fc2c5a2565ba9815e7
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-05-17 07:56:51 +0000

    11주차 과제

---
+++ hw16/test01_word_change.py
@@ -0,0 +1,28 @@
+def toggle_text(text: str) -> str:
+
+    result = ""
+
+    for word in text:
+
+        if word.isalpha():
+
+            if 'A' <= word <= 'Z':
+                result += chr(ord(word) + 32)  # 대문자 -> 소문자
+                
+            elif 'a' <= word <= 'z':
+                result += chr(ord(word) - 32)  # 소문자 -> 대문자
+
+            else:
+                result += word  
+
+    return result
+
+
+def main():
+
+    input_text = input("대/소문자 변환이 필요한 영단어를 입력하시오 : ")
+    print_text = toggle_text(input_text)
+    print(print_text)  
+
+if __name__ == "__main__":
+  main()

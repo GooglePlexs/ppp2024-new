3cc62f396073ef9a26d9cbef5279b553ed7731e5
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-05-25 07:20:41 +0000

    11,12주차 과제

---
+++ hw17_16/hw17_gui_test02_lotto.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import simpledialog
+
+window = tk.Tk()
+window.withdraw()
+
+def gui_input(text: str) -> str:
+    return simpledialog.askstring(title="Test", prompt=text)
+
+import random
+
+
+def get_lotto():
+
+    results = []
+
+    while True:
+        num = random.randint(1, 45)
+        if num not in results:
+            results.append(num)
+        if len(results) == 6:
+           # return results.sort()
+           return sorted(results)
+
+def main():
+
+    count = 5
+
+    for i in range(count):
+        lotto_nums = get_lotto()
+        print(f"{i+1}: {lotto_nums}")
+
+
+if __name__ == "__main__":
+    main()

ddafdde2ff31ab395be641b142105ad0143c1b6e
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-05-28 16:05:57 +0000

    13주차 첫번째 과제

--- hw18/test01_birth_temp.py
+++ hw18/test01_birth_temp.py
@@ -1,29 +1,32 @@
 import csv
 import matplotlib.pyplot as plt
 
-
-f = open('hw18/weather_date.csv')
-data = csv.reader(f)
-
-next(data)
-high = []
-low = []
-year =[]
-
-for row in data :
-    if row[-1] != '' and row[-2] != '':
-        if 1981 <= int(row[0].split('-')[0]) : 
+def main():
+    f = open('hw18/weather_date.csv')
+    data = csv.reader(f)
+    
+    next(data)
+    high = []
+    low = []
+    year =[]
+    
+    for row in data :
+        
+        if row[-1] != '' and row[-2] != '':
+            if 1981 <= int(row[0].split('-')[0]) : 
                 if row[0].split('-')[1] == '05' and row[0].split('-')[2] == '30':
                     high.append(float(row[-2]))
                     low.append(float(row[-1]))
                     year.append(int(row[0].split('-')[0]))
+                    
+    plt.rc('font', family='NanumBarunGothic') 
+    plt.title('생일 기온 변화 그래프')    
 
-plt.rc('font', family='NanumBarunGothic') 
-plt.title('생일 기온 변화 그래프')    
+    plt.plot(year, high, 'r', label='최고')
+    plt.plot(year, low, 'b', label='최저')
+    plt.legend()
 
-plt.plot(year, high, 'r', label='최고')
-plt.plot(year, low, 'b', label='최저')
-plt.legend()
+    plt.savefig("./birth_temp.png")
 
-
-plt.savefig("./birth_temp.png")
+if __name__ == "__main__":
+    main()
--- hw18/test02_spring_winter_temp.py
+++ hw18/test02_spring_winter_temp.py
@@ -1,16 +1,17 @@
 import csv
 import matplotlib.pyplot as plt
 
-f = open('hw18/weather_date.csv')
-data = csv.reader(f)
+def main():
+    f = open('hw18/weather_date.csv')
+    data = csv.reader(f)
 
-next(data)
-winter = []
-summer = []
-
-
-for row in data :
-    month = row[0].split('-')[1]
+    next(data)
+    winter = []
+    summer = []
+    
+    
+    for row in data :
+        month = row[0].split('-')[1]
 
     if row[-1] != '' :
             
@@ -27,15 +28,18 @@ for row in data :
                 summer.append(float(row[-1]))
             if month == '09' :
                 summer.append(float(row[-1]))
+                
+                
+    plt.rc('font', family='NanumBarunGothic') 
+    plt.rcParams['axes.unicode_minus'] = False
 
+    plt.title('겨울철과 여름철의 기온차 분포도')  
 
-plt.rc('font', family='NanumBarunGothic') 
-plt.rcParams['axes.unicode_minus'] = False
+    plt.hist(summer, bins=100, color='r', label='여름철')
+    plt.hist(winter, bins=100, color='b', label='겨울철')
+    plt.legend()
 
-plt.title('겨울철과 여름철의 기온차 분포도')  
+    plt.savefig("./spring_winter_temp.png")
 
-plt.hist(summer, bins=100, color='r', label='여름철')
-plt.hist(winter, bins=100, color='b', label='겨울철')
-plt.legend()
-
-plt.savefig("./spring_winter_temp.png")
+if __name__ == "__main__":
+    main()

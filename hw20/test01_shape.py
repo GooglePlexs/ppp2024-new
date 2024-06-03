40d28b09bf08839dbf2f940437a82a8b30390aa4
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-06-03 06:49:44 +0000

    14주차 첫번째 강의

--- hw20/test01_shape.py
+++ hw20/test01_shape.py
@@ -8,4 +8,14 @@ def main():
         print(shape.area())
         print(shape.perimeter())
 
+from abc import ABC, abstractmethod class Shape(ABC):
+@abstractmethod def area(self):
+pass
+@abstractmethod def perimeter(self):
+pass
+class Rectangle(Shape): def __init__(self, h, v):
+class Triangle(Shape): def __init__(self, h, v):
 
+class Circle(Shape): def __init__(self, r):
+class RegularHexagon(Shape):
+def __init__(self, r):

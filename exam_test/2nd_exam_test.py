a2eb3492a929e9788f496a101e112af60d7c3b62
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-06-20 03:18:21 +0000

    기말과제

--- exam_test/2nd_exam_test.py
+++ exam_test/2nd_exam_test.py
@@ -1,58 +1,90 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import telegram
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+import pandas as pd
 
-url = "https://finance.naver.com/sise/ipo.nhn"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+token = "7258921748:AAE5WbsngK62vocaeJHT4989kvNt8agcdPw"
+id = "7440605409"
+
+bot = telegram.Bot(token)
+bot.sendMessage(chat_id=id, text="공모주 일정 알리미 봇입니다. /공모주 를 입력하셔서 정보를 받아보세요! ^^")
+
+# updater
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
+updater.start_polling()
 
 
+def storage():
+    url = "https://finance.naver.com/sise/ipo.nhn"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    ipo_data = []
+
+    for tr in soup.select("table.type_7 > tbody > tr"):
+        td_list = tr.select("td")
+
+        name = td_list[0].select_one(".item_name")
+        name = name.get_text(strip=True) if name else "미정"
+
+        price = td_list[0].select_one(".area_price .num")
+        price = price.get_text(strip=True) if price else "미정"
+
+        category = td_list[0].select_one(".area_type")
+        category = category.get_text(strip=True) if category else "미정"
+
+        broker = td_list[0].select_one(".area_sup")
+        broker = broker.get_text(strip=True) if broker else "미정"
+
+        competition_rate = td_list[0].select_one(".area_competition .num")
+        competition_rate = competition_rate.get_text(strip=True) if competition_rate else "미정"
+
+        offering_period = td_list[0].select_one(".area_private .num")
+        offering_period = offering_period.get_text(strip=True) if offering_period else "미정"
+
+        listing_date = td_list[0].select_one(".area_list .num")
+        listing_date = listing_date.get_text(strip=True) if listing_date else "미정"
+
+        ipo_entry = {
+            "Name": name,
+            "Price": price,
+            "Category": category,
+            "broker": broker,
+            "Competition Rate": competition_rate,
+            "Offering_period": offering_period,
+            "Listing Date": listing_date,
+        }
+        ipo_data.append(ipo_entry)
+
+    with open("ipo_data.csv", "w", newline="") as csvfile:
+        fieldnames = ["Name", "Price", "Category", "broker", "Competition Rate", "Offering_period", "Listing Date"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(ipo_data)
+
+    print("Results saved to ipo_data.csv")
+
+storage()
 
 
-for tr in soup.select("table.type_7 > tbody > tr"):
+def handler(update, context):
+    user_text = update.message.text
 
-    td_list = tr.select("td")
+    if user_text == "/공모주":
+        # CSV 파일 읽기
+        with open('ipo_data.csv', 'r') as csvfile:
+            # CSV 파일을 DataFrame으로 변환
+            df = pd.read_csv(csvfile)
 
-    name = td_list[0].select_one(".item_name")
-    if name:
-        name = td_list[0].select_one(".item_name").get_text(strip=True)
-    else : 
-        name = "미정"
-    
-    price = td_list[0].select_one(".area_price .num")
-    if price : 
-        price = td_list[0].select_one(".area_price .num").get_text(strip=True)
-    else :
-        price = "미정"
-
-    category = td_list[0].select_one(".area_type")
-    if category : 
-        category = td_list[0].select_one(".area_type").get_text(strip=True)
-    else : 
-        category = "미정"
-
-    underwriter = td_list[0].select_one(".area_sup")
-    if underwriter :
-        underwriter = td_list[0].select_one(".area_sup").get_text(strip=True)
-    else : 
-        underwriter = "미정"
-
-    competition_rate_element = td_list[0].select_one(".area_competition .num")
-    if competition_rate_element:
-        competition_rate = competition_rate_element.get_text(strip=True)
+            # 각 행을 메시지로 보내기
+            for row in df.itertuples():
+                row_text = " ".join(row[1:])  # 각 행의 요소를 공백으로 연결
+                context.bot.send_message(chat_id=id, text=row_text)
     else:
-        copetition_rate = "미정"
+        context.bot.send_message(chat_id=id, text="향후 업데이트 예정입니다.")
 
-    subscription_period = td_list[0].select_one(".area_private .num")
-    if subscription_period :
-        subscription_period = td_list[0].select_one(".area_private .num").get_text(strip=True)
-    else:
-        subscription_period = "미정"
-
-    listing_date = td_list[0].select_one(".area_list .num")
-    if listing_date :
-        listing_date = td_list[0].select_one(".area_list .num").get_text(strip=True)
-    else :
-        listing_date = "미정"
-
-    print(name, price, category, underwriter, competition_rate, subscription_period, listing_date)
+echo_handler = MessageHandler(Filters.text, handler)
+dispatcher.add_handler(echo_handler)
